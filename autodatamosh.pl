714e5e37577b90260ce01e3faad2aa01637b2c9d
Joe Friedl <stuff@joefriedl.net>
2010-04-08 11:21:33 -0500

    Changed default dprob to 0, added licensing info.

--- autodatamosh.pl
+++ autodatamosh.pl
@@ -1,9 +1,20 @@
 #!/usr/bin/perl
 
-# Autodatamosh script copyleft 2009 Joe Friedl
-# http://joefriedl.net
+# Autodatamosh script
+# Copyright 2009, 2010 Joe Friedl <http://joefriedl.net>
 #
-# Released under the GPLv3 license: http://www.gnu.org/licenses/gpl.html
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
 use strict;
 use warnings;
@@ -24,7 +35,7 @@ open(our $outfile,'>&STDOUT');
 my $infilename = '';
 my $outfilename = '';
 
-our $dprob = 0.05;	# P-frame duplication probability
+our $dprob = 0;	# P-frame duplication probability
 
 our $dmin = 10;	# P-frame duplication minimum
 our $dmax = 50; # P-frame duplication maximum
